cc1baae7e386bd3809778a26512e30c1cf291fa3
Anastasia Hamandritov <nasthg96@gmail.com>
2023-02-08 19:16:03 +0200

    adding async local storage

---
+++ app/src/storage/AsyncLocalStorage.js
@@ -0,0 +1,12 @@
+export const AsyncLocalStorage = {
+    setItem: function (key, value) {
+        return Promise.resolve().then(function () {
+            localStorage.setItem(key, value);
+        });
+    },
+    getItem: function (key) {
+        return Promise.resolve().then(function () {
+            return localStorage.getItem(key);
+        });
+    }
+};
