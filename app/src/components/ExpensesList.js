a3f54d2bfa6e42f86932bd96b5db478f3d617db2
Anastasia Hamandritov <nasthg96@gmail.com>
2023-02-08 12:04:43 +0200

    Adding project first steps

---
+++ app/src/components/ExpensesList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Expense from "./Expense";
+import NewExpense from "./NewExpense";
+
+class ExpensesList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { expenses: this.props.expenses};
+  }
+
+  render() {
+    return (
+      <div className="expenses-list">
+        <NewExpense />
+        {this.state.expenses.map((expense) => (
+        <Expense
+          title={expense.title}
+          description={expense.description}
+          category={expense.category}
+          date={expense.date}
+        />
+      ))}
+      </div>
+    );
+  }
+}
+
+export default ExpensesList;
---
+++ app/src/components/Report.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Report extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="report">
+        <h1>Report Placeholder</h1>
+      </div>
+    );
+  }
+}
+
+export default Report;
